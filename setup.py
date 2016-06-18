de449e97647adf91fc1deb8cfd02ab5b22290d7b
martinmaseda <martin.maseda@gmail.com>
2016-06-18 19:52:45 +0200

    First commit 2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='fagroupa',
-      version='0.1',
+      version='0.11',
       description='Try many sklearn algorithms in no time!',
       url='https://github.com/martinmaseda/fagroupa',
       author='Ivan Martin Maseda',
