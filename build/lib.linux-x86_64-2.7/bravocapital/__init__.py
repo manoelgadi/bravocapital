9426f1cb7b68ee530b881b0434fcaf75d9f2807a
mgadi <manoelgadi@gmail.com>
2016-06-22 16:57:18 +0200

    Changes...

---
+++ build/lib.linux-x86_64-2.7/bravocapital/__init__.py
@@ -0,0 +1,9 @@
+def safechars():
+	return 1
+
+def imprime():
+	return 2
+
+def sumauno(valor):
+	return valor+1
+
--- setup2.py
+++ setup2.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='bravocapital',
       version='0.1',
       description='Python package for Bravo Capital!',
-      url='https://github.com/manoelgadi/bravocapital/bravocapital',
+      url='https://github.com/manoelgadi/bravocapital/fagroupa',
       author='Manoel Gadi',
       author_email='manoel.gadi@bravocapital.es',
       license='MIT',
